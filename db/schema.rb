37d65a412d2b1b925ceea3762c6ff76499844749
mt02v <141305346+mt02v@users.noreply.github.com>
2023-12-12 15:22:28 +0000

    [Finish] ツイート投稿失敗した場合のメッセージ表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_09_143608) do
+ActiveRecord::Schema.define(version: 2023_12_12_142107) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["created_at"], name: "index_tweets_on_created_at"
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
